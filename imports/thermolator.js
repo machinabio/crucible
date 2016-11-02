efd809a24fd51c4cfe173c63b133f52b78528d11
dhylbert <hylbert@gmail.com>
2016-11-02 14:12:34 -0700

    wait for Arduino reset before initializing

--- imports/thermolator.js
+++ imports/thermolator.js
@@ -1,19 +1,19 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import '/imports/peripherals.js';
 
 const peripheral_name = 'thermolator';
 
 // Turn off callbacks during initialization
-var intializing = true;
+var initializing = true;
 
 // Initialize the thermolator now
 if (!Peripherals.findOne(peripheral_name)) {
-    Peripherals.insert({
-        _id: peripheral_name,
-        temperature: 25, // in C
-        setpoint: 25, // in C
-        running: false // true = running, false = stopped
-    });
+  Peripherals.insert({
+    _id: peripheral_name,
+    temperature: 25, // in C
+    setpoint: 25, // in C
+    running: false // true = running, false = stopped
+  });
 }
 
 var initSerialPort = function () {
@@ -26,89 +26,90 @@ var initSerialPort = function () {
 };
 
 if (Meteor.settings.thermolator) {
-    var SerialPort = initSerialPort();
+  var SerialPort = initSerialPort();
 
-    const ThermoScientific = (Meteor.settings.thermolator.model.toLowerCase() == 'thermoscientific'); // if this is false, we assume there's a Julabo thermolator connected
-    console.log("thermolator model ", ThermoScientific ? "Thermoscientific" : "Julabo");
+  const ThermoScientific = (Meteor.settings.thermolator.model.toLowerCase() == 'thermoscientific'); // if this is false, we assume there's a Julabo thermolator connected
+  console.log("thermolator model ", ThermoScientific ? "Thermoscientific" : "Julabo");
 
-    var port = new SerialPort.SerialPort(Meteor.settings.thermolator.port, {
-        baudrate: Meteor.settings.thermolator.baudrate,
-        parser: SerialPort.parsers.readline('\r\n')
-    });
+  var port = new SerialPort.SerialPort(Meteor.settings.thermolator.port, {
+    baudrate: Meteor.settings.thermolator.baudrate,
+    parser: SerialPort.parsers.readline('\r\n')
+  });
 
-    port.on('open', function onOpen() {
-      console.log('Thermo port open on:', Meteor.settings.thermolator.port);
-    });
+  port.on('open', function onOpen() {
+    console.log('Thermo port open on:', Meteor.settings.thermolator.port);
+  });
 
-    port.on('data', function (data) {
-      console.log('Thermo Receive:', data);
-    });
+  // port.on('data', function (data) {
+  //   console.log('Thermo Receive');
+  //   console.log('Thermo Received:', data);
+  // });
 
-    port.on('data', Meteor.bindEnvironment(function(data) {
-        let reading = parseFloat(data);
-        if (reading) {
-            Peripherals.update(peripheral_name, {
-                $set: { temperature: reading }
-            });
-            if (Meteor.settings.logging) console.log('Received data from thermolator ', reading);
+  port.on('data', Meteor.bindEnvironment(function (data) {
+    let reading = parseFloat(data);
+    if (reading) {
+      Peripherals.update(peripheral_name, {
+        $set: {temperature: reading}
+      });
+      if (Meteor.settings.logging) console.log('Received data from thermolator ', reading);
+    }
+  }));
+
+  thermolator_off();
+
+  Peripherals.find(peripheral_name).observeChanges({
+    changed: function changed(id, fields) {
+      if (initializing) return;
+      let thermolator = Peripherals.findOne(id);
+      let changed_fields = Object.getOwnPropertyNames(fields);
+      for (let field of changed_fields) {
+        switch (field) {
+          case 'running':
+            if (Meteor.settings.logging) console.log('toggled thermolator running');
+            fields.running ? thermolator_on() : thermolator_off();
+            break;
+          case 'setpoint':
+            if (Meteor.settings.logging) console.log('updated thermolator setpoint');
+            update_setpoint();
+            break;
+          default:
+          // no relevant fields were changed
         }
-    }));
+      }
+    }
+  });
 
-    thermolator_off();
+  Meteor.setInterval(get_temperature, 1000);
 
-    Peripherals.find(peripheral_name).observeChanges({
-        changed: function changed(id, fields) {
-            if (initializing) return;
-            let thermolator = Peripherals.findOne(id);
-            let changed_fields = Object.getOwnPropertyNames(fields);
-            for (let field of changed_fields) {
-                switch (field) {
-                    case 'running':
-                        if (Meteor.settings.logging) console.log('toggled thermolator running');
-                        fields.running ? thermolator_on() : thermolator_off();
-                        break;
-                    case 'setpoint':
-                        if (Meteor.settings.logging) console.log('updated thermolator setpoint');
-                        update_setpoint();
-                        break;
-                    default:
-                        // no relevant fields were changed
-                }
-            }
-        }
-    });
+  // functions are hoisted
+  function update_setpoint() {
+    let tempSet = Peripherals.findOne(peripheral_name).setpoint;
+    let messageThermo = ThermoScientific ? 'W SP ' : 'out_sp_00 ';
+    messageThermo += tempSet;
+    send_to_thermolator(messageThermo);
+  }
 
-    Meteor.setInterval(get_temperature, 1000);
+  function thermolator_off() {
+    let command = ThermoScientific ? 'W RR -1' : 'out_mode_05 0';
+    send_to_thermolator(command);
+  }
 
-    // functions are hoisted
-    function update_setpoint() {
-        let tempSet = Peripherals.findOne(peripheral_name).setpoint;
-        let messageThermo = ThermoScientific ? 'W SP ' : 'out_sp_00 ';
-        messageThermo += tempSet;
-        send_to_thermolator(messageThermo);
-    };
+  function thermolator_on() {
+    let command = ThermoScientific ? 'W GO 1' : 'out_mode_05 1';
+    send_to_thermolator(command);
+  }
 
-    function thermolator_off() {
-        let command = ThermoScientific ? 'W RR -1' : 'out_mode_05 0';
-        send_to_thermolator(command);
-    };
+  function get_temperature() {
+    let messageThermo = ThermoScientific ? 'R T1' : 'in_pv_00';
+    send_to_thermolator(messageThermo);
+  }
 
-    function thermolator_on() {
-        let command = ThermoScientific ? 'W GO 1' : 'out_mode_05 1';
-        send_to_thermolator(command);
-    };
-
-    function get_temperature() {
-        let messageThermo = ThermoScientific ? 'R T1' : 'in_pv_00';
-        send_to_thermolator(messageThermo);
-    };
-
-    function send_to_thermolator(message) {
-        if (port.isOpen()) {
-            port.write(message + '\r\n');
-        }
-        if (Meteor.settings.logging) console.log('Sending data to thermolator ', message);
-    };
+  function send_to_thermolator(message) {
+    if (port.isOpen()) {
+      port.write(message + '\r\n');
+      if (Meteor.settings.logging) console.log('Sending data to thermolator ', message);
+    }
+  }
 }
 
 initializing = false;
